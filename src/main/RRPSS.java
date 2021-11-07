b2a3db821388ad5e3ee37ab1f5c6f996a8dadc05
Randomystick <bryankhleow@gmail.com>
2021-11-07 17:44:48 +0800

    no functional changes

--- src/main/RRPSS.java
+++ src/main/RRPSS.java
@@ -10,12 +10,22 @@ public class RRPSS {
 		Scanner sc = new Scanner(System.in);
 		int choice = -1;
 		do {
-			System.out.println("(1) Edit Menu Item Catalog\n" + "(2) Edit Promotion Catalog\n" + "(3) Edit Menus\n"
-					+ "(4) Create Order\n" + "(5) View Order\n" + "(6) Add/Remove Order Item(s) to/from Order\n"
-					+ "(7) Create Reservation Booking\n" + "(8) Check/Remove Reservation Booking\n"
-					+ "(9) Check Table Availability\n" + "(10) Print Order Invoice\n"
-					+ "(11) Print Sale Revenue Report\n" + "(12) Add in staff details\n" + "(13) Add tables\n"
-					+ "(-1) Exit");
+			System.out.println(
+					"(1) Edit Menu Item Catalog\n" + 
+					"(2) Edit Promotion Catalog\n" + 
+					"(3) Edit Menus\n" + 
+					"(4) Create Order\n" + 
+					"(5) View Order\n" + 
+					"(6) Add/Remove Order Item(s) to/from Order\n" +
+					"(7) Create Reservation Booking\n" +
+					"(8) Check/Remove Reservation Booking\n" +
+					"(9) Check Table Availability\n" +
+					"(10) Print Order Invoice\n" +
+					"(11) Print Sale Revenue Report\n" +
+					"(12) Add in staff details\n" +
+					"(13) Add tables\n" +
+					"(-1) Exit"
+			);
 			System.out.println("Enter your Selection: ");
 			choice = sc.nextInt();
 			sc.nextLine();
