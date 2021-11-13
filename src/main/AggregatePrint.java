e5ec798663b77d2cd6e273fa9664117a9fcb4301
Malthus Oh <malthusoh@gmail.com>
2021-11-13 19:16:01 +0800

    updates

--- src/main/AggregatePrint.java
+++ src/main/AggregatePrint.java
@@ -5,72 +5,72 @@ import java.util.ArrayList;
 import main.Menu.MenuItem;
 
 /**
- * An abstract class defining an object that does three things:
- * 1) Processes an ungrouped array of objects including MenuItems
- * 2) Aggregates MenuItems with Quantity.
- * 3) Prints those MenuItems with Quantity.
- * This abstract class will be used by the OrderInvoiceApp and SalesReportApp to process, aggregate and print.
- * They share common attributes such as an array of MenuItems to print and an array indicating quantity,
- * and a common method to aggregate MenuItems with Quantity, but their processing to extract the relevant
- * MenuItems and their printing are different (e.g. OrderInvoice needs to show table number and time and discounts etc,
- * but SalesReport only needs the revenue)
+ * An abstract class defining an object that does three things: 1) Processes an
+ * ungrouped array of objects including MenuItems 2) Aggregates MenuItems with
+ * Quantity. 3) Prints those MenuItems with Quantity. This abstract class will
+ * be used by the OrderInvoiceApp and SalesReportApp to process, aggregate and
+ * print. They share common attributes such as an array of MenuItems to print
+ * and an array indicating quantity, and a common method to aggregate MenuItems
+ * with Quantity, but their processing to extract the relevant MenuItems and
+ * their printing are different (e.g. OrderInvoice needs to show table number
+ * and time and discounts etc, but SalesReport only needs the revenue)
+ * 
  * @author Bryan
  * @version 1.0
  */
 public abstract class AggregatePrint {
 	protected static ArrayList<MenuItem> grpedSaleItems;
 	protected static int[] grpedSaleQty;
-	
+
 	/**
 	 * Public constructor to initialise the array list of MenuItems for aggregating
 	 */
 	public AggregatePrint() {
 		grpedSaleItems = new ArrayList<MenuItem>();
 	}
-	
+
 	/**
 	 * Abstract method to process an object to get ready for grouping Sale Items.
 	 * See the subclasses for their exact implementation
 	 */
 	protected abstract void process();
-	
+
 	/**
-	 * Method that:
-	 * 1) Reads in an array list of ungrouped Menu Items, i.e. has duplicates
-	 * 2) Groups them and remembers the quantity for each Menu Item
+	 * Method that: 1) Reads in an array list of ungrouped Menu Items, i.e. has
+	 * duplicates 2) Groups them and remembers the quantity for each Menu Item
+	 * 
 	 * @param array list of menu items ungroupedSaleItems
 	 */
 	protected static void groupSaleItems(ArrayList<MenuItem> ungroupedSaleItems) {
 		grpedSaleItems.clear();
-		for (MenuItem ungroupedItem : ungroupedSaleItems)
-		{
+		grpedSaleItems.add(ungroupedSaleItems.get(0));
+		for (MenuItem ungroupedItem : ungroupedSaleItems) {
 			String curName = ungroupedItem.getName();
-			for (MenuItem grpedItem : grpedSaleItems)
-			{
-				if (grpedItem.getName().equals(curName))
-				{
-					grpedSaleItems.add(ungroupedItem);
+			boolean exists = false;
+			for (MenuItem grpedItem : grpedSaleItems) {
+				if (grpedItem.getName().equals(curName)) {
+					exists = true;
+					break;
 				}
 			}
+			if (!exists)
+				grpedSaleItems.add(ungroupedItem);
 		}
 		grpedSaleQty = new int[grpedSaleItems.size()];
-		for (int i = 0; i < grpedSaleItems.size(); i++)
-		{
+		for (int i = 0; i < grpedSaleItems.size(); i++) {
 			MenuItem cur = grpedSaleItems.get(i);
-			for (MenuItem ungroupedItem : ungroupedSaleItems)
-			{
-				if (ungroupedItem.getName().equals(cur.getName()))
-				{
+			for (MenuItem ungroupedItem : ungroupedSaleItems) {
+				if (ungroupedItem.getName().equals(cur.getName())) {
 					grpedSaleQty[i]++;
 				}
 			}
 		}
 	}
-	
+
 	/**
-	 * Abstract method to print out the Order Invoice / Sale Report.
-	 * See the subclasses for their exact implementation.
+	 * Abstract method to print out the Order Invoice / Sale Report. See the
+	 * subclasses for their exact implementation.
 	 */
 	protected abstract void print();
-	
+
 }
--- src/main/SalesReportApp.java
+++ src/main/SalesReportApp.java
@@ -11,12 +11,12 @@ import main.Order.OrderInvoice;
 import main.IO.*;
 
 /**
- * Represents the Sales Report App class that is responsible for
- * 1) Receiving the order invoices
- * 2) Allowing the user to choose the time period for the reservation
- * 3) GROUPING THE SALE ITEMS WITH QUANTITY 
- * 4) Printing it in the Order Invoice format.
- * It is a concrete class that inherits from the AggregatePrint abstract class.
+ * Represents the Sales Report App class that is responsible for 1) Receiving
+ * the order invoices 2) Allowing the user to choose the time period for the
+ * reservation 3) GROUPING THE SALE ITEMS WITH QUANTITY 4) Printing it in the
+ * Order Invoice format. It is a concrete class that inherits from the
+ * AggregatePrint abstract class.
+ * 
  * @author Bryan
  * @version 1.0
  */
@@ -29,8 +29,8 @@ public class SalesReportApp extends AggregatePrint implements RW {
 	 * An attribute to calculate total revenue
 	 */
 	private double totalRevenue;
-	private final String filename = "orderinvoices"; 
-	
+	private final String filename = "orderinvoices";
+
 	/**
 	 * Public constructor to create sales report app and initailise revenue to 0
 	 */
@@ -39,67 +39,56 @@ public class SalesReportApp extends AggregatePrint implements RW {
 		grpedSaleItems = new ArrayList<MenuItem>();
 		totalRevenue = 0;
 	}
-	
+
 	/**
 	 * Method to add an order invoice to the report
+	 * 
 	 * @param OrderInvoice object orderInvoice
 	 */
 	public static void addToReport(OrderInvoice orderInvoice) {
 		orderInvoices.add(orderInvoice);
 	}
-	
+
 	/**
-	 * Method to process a Sales Report. This is done by
-	 * 1) Prompting the user which time period they want
-	 * 2) Calling the appropriate overloaded processReport method
+	 * Method to process a Sales Report. This is done by 1) Prompting the user which
+	 * time period they want 2) Calling the appropriate overloaded processReport
+	 * method
+	 * 
 	 * @Override
 	 */
 	public void process() {
 		System.out.println("By (1) Day (2) Month");
 		int choice = ErrorApp.safeInteger();
-		if (choice == 1)
-		{
+		if (choice == 1) {
 			System.out.println("Input date (eg. 2021-07-30):");
-	        LocalDate date = ErrorApp.dateHandlerAfter();
-	        processReport(date);
-		}
-		else if (choice == 2)
-		{
-			System.out.println("Select month: (-1 to return)\n"
-							 + "(1) January\n"
-							 + "(2) Feburary\n"
-							 + "(3) March\n"
-							 + "(4) April\n"
-							 + "(5) May\n"
-							 + "(6) June\n"
-							 + "(7) July\n"
-							 + "(8) August\n"
-							 + "(9) September\n"
-							 + "(10) October\n"
-							 + "(11) November\n"
-							 + "(12) December");
-	        int month = ErrorApp.safeInteger();
-	        if (month == -1) return;
-	        else if (1 > month || month > 12)
-	        {
-	        	System.out.println("Invalid choice");
-	        }
-	        else processReport(month);
-		}
-		else if (choice == -1) return;
+			LocalDate date = ErrorApp.dateHandlerAfter();
+			processReport(date);
+		} else if (choice == 2) {
+			System.out.println("Select month: (-1 to return)\n" + "(1) January\n" + "(2) Feburary\n" + "(3) March\n"
+					+ "(4) April\n" + "(5) May\n" + "(6) June\n" + "(7) July\n" + "(8) August\n" + "(9) September\n"
+					+ "(10) October\n" + "(11) November\n" + "(12) December");
+			int month = ErrorApp.safeInteger();
+			if (month == -1)
+				return;
+			else if (1 > month || month > 12) {
+				System.out.println("Invalid choice");
+			} else
+				processReport(month);
+		} else if (choice == -1)
+			return;
 	}
 
 	/**
-	 * Overloaded method to process a Sales Report. It checks whether the menuitem is within the time period.
+	 * Overloaded method to process a Sales Report. It checks whether the menuitem
+	 * is within the time period.
+	 * 
 	 * @Override
 	 */
 	private void processReport(int month) {
 		ArrayList<MenuItem> ungroupedSaleItems = new ArrayList<MenuItem>();
 		totalRevenue = 0;
-		for (OrderInvoice orderinvoice : orderInvoices)
-		{
-			if (month == orderinvoice.getInvoiceDate().getMonthValue())
-			{
+		for (OrderInvoice orderinvoice : orderInvoices) {
+			if (month == orderinvoice.getInvoiceDate().getMonthValue()) {
 				ungroupedSaleItems.addAll(orderinvoice.getOrderItems());
 				totalRevenue += orderinvoice.getRevenue();
 			}
@@ -110,16 +99,16 @@ public class SalesReportApp extends AggregatePrint implements RW {
 	}
 
 	/**
-	 * Overloaded method to process a Sales Report. It checks whether the menuitem is within the time period.
+	 * Overloaded method to process a Sales Report. It checks whether the menuitem
+	 * is within the time period.
+	 * 
 	 * @Override
 	 */
 	private void processReport(LocalDate date) {
 		totalRevenue = 0;
 		ArrayList<MenuItem> ungroupedSaleItems = new ArrayList<MenuItem>();
-		for (OrderInvoice orderinvoice : orderInvoices)
-		{
-			if (date.isEqual(orderinvoice.getInvoiceDate()))
-			{
+		for (OrderInvoice orderinvoice : orderInvoices) {
+			if (date.isEqual(orderinvoice.getInvoiceDate())) {
 				ungroupedSaleItems.addAll(orderinvoice.getOrderItems());
 				totalRevenue += orderinvoice.getRevenue();
 			}
@@ -128,24 +117,22 @@ public class SalesReportApp extends AggregatePrint implements RW {
 		System.out.println("Sales Revenue Report (for " + date + ")");
 		print();
 	}
-	
+
 	/**
 	 * Mthod to print a Sales Report in the proper format
+	 * 
 	 * @Override
 	 */
 	protected void print() {
-		if (totalRevenue == 0)
-		{
+		if (totalRevenue == 0) {
 			System.out.println("No sales during this period");
 			return;
 		}
-		System.out.printf( "Total revenue is $%.2f\n", totalRevenue);
-		System.out.println("\n" + String.format("%-26s %6s", "Item", "Qty")
-					   	 + "\n" + String.format("%-26s %6s", "----", "---")
-					   	 );
+		System.out.printf("Total revenue is $%.2f\n", totalRevenue);
+		System.out.println(
+				"\n" + String.format("%-26s %6s", "Item", "Qty") + "\n" + String.format("%-26s %6s", "----", "---"));
 		int i = 0;
-		for (MenuItem saleItem : grpedSaleItems)
-		{
+		for (MenuItem saleItem : grpedSaleItems) {
 			System.out.println(String.format("%-27s %5s\n", saleItem.getName(), grpedSaleQty[i++]));
 		}
 	}
@@ -155,29 +142,27 @@ public class SalesReportApp extends AggregatePrint implements RW {
 		// TODO Auto-generated method stub
 		IO.setFileName(this.filename);
 		IO.setWriter();
-		
-		for(int i = 0; i < orderInvoices.size(); i++)
-		{
+
+		for (int i = 0; i < orderInvoices.size(); i++) {
 			OrderInvoice oi = orderInvoices.get(i);
-			IO.write(oi.getInvoiceDate()+",");
-			IO.write(oi.getInvoiceTime()+",");
-			IO.write(oi.getTable()+",");
-			IO.write(oi.getInitialPrice()+",");
-			IO.write(oi.getDiscounted()+",");
-			IO.write(oi.getRevenue()+",");
-			IO.write(oi.getFinalPrice()+"\n");
-			for (int j = 0; j < oi.getOrderItems().size(); j++)
-			{
+			IO.write(oi.getInvoiceDate() + ",");
+			IO.write(oi.getInvoiceTime() + ",");
+			IO.write(oi.getTable() + ",");
+			IO.write(oi.getInitialPrice() + ",");
+			IO.write(oi.getDiscounted() + ",");
+			IO.write(oi.getRevenue() + ",");
+			IO.write(oi.getFinalPrice() + "\n");
+			for (int j = 0; j < oi.getOrderItems().size(); j++) {
 				MenuItem mi = oi.getOrderItems().get(j);
-				IO.write(mi.getName() + "§");
-				IO.write(mi.getPrice() + "§");
-				IO.write(mi.getType() + "§");
+				IO.write(mi.getName() + "ï¿½");
+				IO.write(mi.getPrice() + "ï¿½");
+				IO.write(mi.getType() + "ï¿½");
 				IO.write(mi.getDescription() + "\n");
 			}
 			IO.write("-----\n");
 		}
 		IO.closeWriter();
-		
+
 	}
 
 	@Override
@@ -185,12 +170,11 @@ public class SalesReportApp extends AggregatePrint implements RW {
 		// TODO Auto-generated method stub
 		IO.setFileName(filename);
 		IO.setReader();
-		
-		if(IO.checkFileExist())
-		{
+
+		if (IO.checkFileExist()) {
 			System.out.println("Order Invoice backup file exists, restoring now.");
-			IO.readLine(); //reads the first line of the text
-			while(!IO.isEOL()) //as long as we are not at the end of line
+			IO.readLine(); // reads the first line of the text
+			while (!IO.isEOL()) // as long as we are not at the end of line
 			{
 				String[] data = IO.getLine().split(","); // takes line and split by comma
 				LocalDate date = LocalDate.parse(data[0], DateTimeFormatter.ofPattern("uuuu-M-d"));
@@ -202,9 +186,8 @@ public class SalesReportApp extends AggregatePrint implements RW {
 				double finalPrice = Double.parseDouble(data[6]);
 				ArrayList<MenuItem> orderItems = new ArrayList<MenuItem>();
 				IO.readLine(); // get the next line
-				while (!IO.isEOL() && !IO.getLine().equals("-----"))
-				{
-					String[] menuItemData = IO.getLine().split("§");
+				while (!IO.isEOL() && !IO.getLine().equals("-----")) {
+					String[] menuItemData = IO.getLine().split("ï¿½");
 					String name = menuItemData[0];
 					double price = Double.parseDouble(menuItemData[1]);
 					String type = menuItemData[2];
@@ -213,13 +196,14 @@ public class SalesReportApp extends AggregatePrint implements RW {
 					orderItems.add(mi);
 					IO.readLine();
 				}
-				OrderInvoice oi = new OrderInvoice(date, time, table, initialPrice, discounted, revenue, finalPrice, orderItems); 
+				OrderInvoice oi = new OrderInvoice(date, time, table, initialPrice, discounted, revenue, finalPrice,
+						orderItems);
 				orderInvoices.add(oi);
 				IO.readLine();
 			}
 		}
-		
+
 		IO.closeReader();
-		
+
 	}
 }
